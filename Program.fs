60d3e53ae09ef1da0ca7af5233d8cc0596635907
SofaKB <hall.b17@gmail.com>
2020-02-09 17:13:37 -0500

    file dump

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Suave
+open App
+
+let hostName = System.Net.IPAddress.Parse "0.0.0.0"
+
+DotLiquid.setTemplatesDir (sprintf "%s/templates/" __SOURCE_DIRECTORY__)
+DotLiquid.setCSharpNamingConvention()
+
+[<EntryPoint>]
+let main argv =
+    startWebServer { defaultConfig with bindings = [ HttpBinding.create HTTP hostName 8888us ] } App.Router
+    0 // return an integer exit code
